43d65b9ed489a1a2562984fca41845f95f171989
Kutzma Michael <KUT11083@dslocal.com>
2017-11-27 16:22:55 -0500

    tweak features

--- randomforest/highlowclassifier.py
+++ randomforest/highlowclassifier.py
@@ -3,18 +3,19 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 
+print("Importing Training Data...")
 train_data = pd.read_table("../input/train.tsv")
 highlowprice = 100
 target = np.array([(0 if x>highlowprice else 1) for x in train_data.price])
 
-'''
-trying to decide on what to do about the 'name' category.
-basically, I'd like to add some tagging features like you would do in nlp.
-maybe check for colors, sizes, clothing type, or grab some most common words
-'''
-train_data = train_data[['category_name','brand_name']]
+# Initial Columns:
+# train_id, name, item_condition_id, category_name, brand_name,
+# price, shipping, item_description
+
+train_data = train_data[['name','item_condition_id','category_name','brand_name']]
 train_data = train_data.fillna('missing')
 
+print("Encoding Data...")
 encoders = {}
 for c in train_data.columns:
     encoders[c] = LabelEncoder()
@@ -27,9 +28,10 @@ Xtrain,Xval,Xtest = train_data.values[:ts[0]],\
         train_data.values[ts[1]] 
 ytrain,yval,ytest = target[:ts[0]],target[ts[0]:ts[1]],target[ts[1]]
 
+print("Training Model...")
 clf = RandomForestClassifier()
 clf.fit(Xtrain,ytrain)
-#print(clf.feature_importances_)
+featimportance = clf.feature_importances_
 ypred = clf.predict(Xval)
 
 accuracy = np.sum([i==j for i,j in zip(ypred,yval)])/len(yval)
@@ -41,4 +43,6 @@ print('Overall Accuracy: ',accuracy)
 print('Low Price Accuracy: ',lowacc)
 print('High Price Accuracy: ',highacc)
 
-
+print("Feature Importance:")
+for f,v in zip(train_data.columns,featimportance):
+    print(' ',f,': ',v)
